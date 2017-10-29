131cd6cbe10f10a87a4f69d4b5daf9777876f8e9
Alex Bouma <me@alexbouma.me>
2017-10-29 21:50:13 +0100

    Move raven js to public dir

--- src/class-wp-sentry-js-tracker.php
+++ src/class-wp-sentry-js-tracker.php
@@ -99,7 +99,7 @@ final class WP_Sentry_Js_Tracker extends WP_Sentry_Tracker_Base {
 	public function on_enqueue_scripts() {
 		wp_enqueue_script(
 			'wp-sentry-raven',
-			plugin_dir_url( WP_SENTRY_PLUGIN_FILE ) . 'raven/js/raven-3.17.0.min.js',
+			plugin_dir_url( WP_SENTRY_PLUGIN_FILE ) . 'public/raven-3.17.0.min.js',
 			[ 'jquery' ],
 			'3.17.0'
 		);
