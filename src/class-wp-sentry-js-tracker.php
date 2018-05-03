c401a5bbf06f01cf79beb7540b978b5feb7c0e31
Alex Bouma <me@alexbouma.me>
2018-05-03 15:21:17 +0200

    Update Raven JS to version 3.24.2

--- src/class-wp-sentry-js-tracker.php
+++ src/class-wp-sentry-js-tracker.php
@@ -99,9 +99,9 @@ final class WP_Sentry_Js_Tracker extends WP_Sentry_Tracker_Base {
 	public function on_enqueue_scripts() {
 		wp_enqueue_script(
 			'wp-sentry-raven',
-			plugin_dir_url( WP_SENTRY_PLUGIN_FILE ) . 'public/raven-3.22.0.min.js',
+			plugin_dir_url( WP_SENTRY_PLUGIN_FILE ) . 'public/raven-3.24.2.min.js',
 			[],
-			'3.22.0'
+			'3.24.2'
 		);
 
 		wp_localize_script(
