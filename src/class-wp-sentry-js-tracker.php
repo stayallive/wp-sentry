32c38428e0bad0aa48174f77dbcf00b650a7ab5a
Alex Bouma <me@alexbouma.me>
2018-11-19 11:09:27 +0100

    Update Sentry Browser to version 4.3.2

--- src/class-wp-sentry-js-tracker.php
+++ src/class-wp-sentry-js-tracker.php
@@ -99,9 +99,9 @@ final class WP_Sentry_Js_Tracker extends WP_Sentry_Tracker_Base {
 	public function on_enqueue_scripts() {
 		wp_enqueue_script(
 			'wp-sentry-browser',
-			plugin_dir_url( WP_SENTRY_PLUGIN_FILE ) . 'public/sentry-browser-4.2.3.min.js',
+			plugin_dir_url( WP_SENTRY_PLUGIN_FILE ) . 'public/sentry-browser-4.3.2.min.js',
 			[],
-			'4.2.3'
+			'4.3.2'
 		);
 
 		wp_localize_script(
