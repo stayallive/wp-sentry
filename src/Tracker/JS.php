ea5fb9c27194fa864c7ce9276f96a705a5522b37
Christopher Hahn <chris@creativefuse.org>
2019-05-03 11:14:19 -0400

    Replace Old  JS Tracker Class
    - Simplified javascript class that is only responsible for enqueuing and localizing scripts, and hydrating contexts if that is needed.
    
    - This new class implements constants to control file versioning and enqueue versioning references. This will ensure that the file cache gets cleared on each new version. Even if the browser script stays the same, we may have to make a change to that file which would ultimately might not show up due to caching.

---
+++ src/Tracker/JS.php
@@ -0,0 +1,93 @@
+<?php
+namespace WPSentry\Tracker;
+
+use WPSentry\Config\ConfigInterface;
+use WPSentry\Context\Context;
+use WPSentry\Tracker\TrackerBase;
+
+/**
+ * Implement Sentry.io for Javascript
+ *
+ * @package WPSentry\Tracker;
+ * @since 3.0.0
+ */
+class JS extends TrackerBase{
+
+  /**
+   * Bootstrap the tracker
+   *
+   * Define enqueue actions where our Javascript needs to be loaded
+   *
+   * @since 3.0.0
+   */
+	protected function bootstrap(){
+
+    // Register on front-end using the highest priority.
+		add_action( 'wp_enqueue_scripts', [ $this, 'load_sentry_js' ], 0 );
+
+		// Register on admin using the highest priority.
+		add_action( 'admin_enqueue_scripts', [ $this, 'load_sentry_js' ], 0 );
+
+		// Register on login using the highest priority.
+		add_action( 'login_enqueue_scripts', [ $this, 'load_sentry_js' ], 0 );
+
+  }
+
+  /**
+   * Enqueue the plugin-defined version of sentry-browser.js and then localize the
+   * script with runtime configuration options and the currently defined context.
+   *
+   * Note that contexts should be hydrated at this point, but if they are not
+   * they will get hydrated here. We are safe to attempt this, as contexts
+   * will only ever be hydrated once per page lifecycle no matter how many
+   * times the hydration methods are called.
+   *
+   * @link https://docs.sentry.io/enriching-error-data/context/?platform=php#capturing-the-user
+   * @since 3.0.0
+   */
+  public function load_sentry_js(){
+
+    // If any contexts have not yet been hydrated, they will be hydrated here
+    $this->context->hydrate_user_context();
+    $this->context->hydrate_tags_context();
+    $this->context->hydrate_extra_context();
+
+    wp_enqueue_script(
+      'wp-sentry-browser',
+      WP_SENTRY_PLUGIN_DIR_URL . 'public/sentry-browser-' . WP_SENTRY_SCRIPT_VERSION . '.min.js',
+      [],
+			WP_SENTRY_PLUGIN_VERSION
+    );
+
+    wp_localize_script(
+      'wp-sentry-browser',
+      'wp_sentry',
+      [
+        $this->get_init_config(),
+        $this->context->config->get(),
+      ]
+    );
+
+  }
+
+  /**
+   * Get initial runtime configurations to pass along to Sentry.
+   *
+   * Available Filter: 'wp_sentry_public_options' - allow plugins to manage their own context.
+   *
+   * @uses array_filter() to clean out empty properties from the array before returning.
+   * @return array
+   */
+  protected function get_init_config() : array{
+
+    // Get user init options
+    $init_config = (array) apply_filters( 'wp_sentry_public_options', $this->init_config->get() );
+
+    // Clean out empty array properties.
+    $init_config = (array) array_filter( $init_config );
+
+    return $init_config;
+
+  }
+
+}
