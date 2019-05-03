e2048e983d548de22a2a3d75d67cf234e32bddc4
Christopher Hahn <chris@creativefuse.org>
2019-05-03 11:09:40 -0400

    Only Allow Context to Be Hydrated Once + Add Comments
    - Since the context is being shared across trackers, we are making sure it only ever gets called ocne by implementing a recursive filter that runs inside of the hydrations method after the method runs. This prevents subsequent calls to the hydration methods from having any effect.
    
    - Update documentation to make it more clear what filters are available within the methods.

--- src/Context/Context.php
+++ src/Context/Context.php
@@ -32,27 +32,34 @@ class Context{
    * Hydrate the user context with data based on the current logged-in user.
    * If this data is not available, nothing will be added to the context.
    *
-   * This context is filterable via `wp_sentry_user_context` to allow plugins to
-   * manage their own context. ie. members plugin.
-   *
    * @link https://docs.sentry.io/enriching-error-data/context/?platform=php#capturing-the-user
+   *
+   * Available Filter: `wp_sentry_user_context` - allow plugins to manage their own context. ie. members plugin.
+   * Available Filter: `wp_sentry_user_context_hydrated` - control hydration state
+   *
    * @uses wp_get_current_user() to get the currently logged-in user
    * @uses array_filter() to clean out empty properties from the array before returning.
    * @since 3.0.0
    */
 	public function hydrate_user_context(){
 
-    $current_user = wp_get_current_user();
+    $is_hydrated = apply_filters( 'wp_sentry_user_context_hydrated', false );
 
-		// Bail if we can't verify the user
-		if ( ! $current_user instanceof \WP_User || ! $current_user->exists() )
+    // Only hydrate the context once, otherwise bail
+    if( $is_hydrated === true )
       return;
 
-		$user_context = [
+    $current_user = wp_get_current_user();
 
-			'id'       		=> $current_user->ID,
-			'name'     		=> $current_user->display_name,
-			'email'    		=> $current_user->user_email,
+    // Bail if we can't verify the user
+    if ( ! $current_user instanceof \WP_User || ! $current_user->exists() )
+      return;
+
+    $user_context = [
+
+      'id'       		=> $current_user->ID,
+      'name'     		=> $current_user->display_name,
+      'email'    		=> $current_user->user_email,
       'username' 		=> $current_user->user_login,
 
     ];
@@ -64,13 +71,16 @@ class Context{
     // Update our context configuration with new data
     $this->config->push( 'user', $user_context );
 
+    // This context has been hydrated
+    add_filter( 'wp_sentry_user_context_hydrated', '__return_true' );
+
 	}
 
   /**
    * Hydrate the tags context with data based on the current environment
    *
-   * This context is filterable via `wp_sentry_tags_context` to allow plugins and
-   * themes to manage their own context.
+   * Available Filter: `wp_sentry_tags_context` - allow plugins to manage their own context.
+   * Available Filter: `wp_sentry_tags_context_hydrated` - control hydration state
    *
    * @link https://docs.sentry.io/enriching-error-data/context/?platform=php#tagging-events
    * @uses array_filter() to clean out empty properties from the array before returning.
@@ -78,11 +88,17 @@ class Context{
    */
 	public function hydrate_tags_context(){
 
+    $is_hydrated = apply_filters( 'wp_sentry_tags_context_hydrated', false );
+
+    // Only hydrate the context once, otherwise bail
+    if( $is_hydrated === true )
+      return;
+
     $tags_context = [
 
       'language'    => get_bloginfo( 'language' ),
-			'wordpress'		=> get_bloginfo( 'version' ),
-			'woocommerce'	=> defined( 'WC_VERSION' ) ? WC_VERSION : '',
+      'wordpress'		=> get_bloginfo( 'version' ),
+      'woocommerce'	=> defined( 'WC_VERSION' ) ? WC_VERSION : '',
       'php'			    => PHP_VERSION,
 
     ];
@@ -93,7 +109,10 @@ class Context{
     $tags_context = (array) array_filter( $tags_context );
 
     // Update our context configuration with new data
-		$this->config->push( 'tags', $tags_context );
+    $this->config->push( 'tags', $tags_context );
+
+    // This context has been hydrated
+    add_filter( 'wp_sentry_tags_context_hydrated', '__return_true' );
 
   }
 
@@ -101,14 +120,20 @@ class Context{
    * Hydrate the tags context with data based on defined "extra context"
    * If this data is not available, nothing will be added to the context
    *
-   * This context is filterable via `wp_sentry_extra_context` to allow plugins and
-   * themes to manage their own context.
+   * Available Filter: `wp_sentry_extra_context` - allow plugins to manage their own context.
+   * Available Filter: `wp_sentry_extra_context_hydrated` - control hydration state
    *
    * @link https://docs.sentry.io/enriching-error-data/context/?platform=php#extra-context
    * @uses array_filter() to clean out empty properties from the array before returning.
    * @since 3.0.0
    */
   public function hydrate_extra_context(){
+
+    $is_hydrated = apply_filters( 'wp_sentry_extra_context_hydrated', false );
+
+    // Only hydrate the context once, otherwise bail
+    if( $is_hydrated === true )
+      return;
 
     $extra_context = [];
 
@@ -117,7 +142,10 @@ class Context{
     $extra_context = (array) array_filter( $extra_context );
 
     // Update our context configuration with new data
-		$this->config->push( 'extra', $extra_context );
+    $this->config->push( 'extra', $extra_context );
+
+    // This context has been hydrated
+    add_filter( 'wp_sentry_extra_context_hydrated', '__return_true' );
 
   }
 
