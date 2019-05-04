09cfbd1d66f6fed2dfda6bcc00f78d16bd9e74f4
Christopher Hahn <chris@creativefuse.org>
2019-05-04 16:48:27 -0400

    Simplify Plugin File
    - The plugin file itself is now super light weight. We will hardly ever have to touch it except for version bumps.

--- wp-sentry.php
+++ wp-sentry.php
@@ -15,8 +15,8 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// If the plugin was already loaded as a mu-plugin do not load again.
-if ( defined( 'WP_SENTRY_MU_LOADED' ) ) {
+// If the plugin was already loaded, do not load again.
+if ( defined( 'WP_SENTRY_PRESENT' ) ) {
 	return;
 }
 
@@ -34,79 +34,20 @@ defined( 'WP_SENTRY_PLUGIN_VERSION' ) || define( 'WP_SENTRY_PLUGIN_VERSION', '3.
  */
 defined ( 'WP_SENTRY_MIN_PHP_VERSION' ) || define( 'WP_SENTRY_MIN_PHP_VERSION', '7.0' );
 
-/**
- * Define a Text Domain for WP Sentry.
- *
- * @since 3.0.0
- */
-defined( 'WP_SENTRY_TEXT_DOMAIN' ) || define( 'WP_SENTRY_TEXT_DOMAIN', 'wp-sentry' );
 
 /**
- * Resolve the Sentry Plugin File
+ * Resolve the WP Sentry Plugin File
  *
  * @since 1.0.0
  */
 defined( 'WP_SENTRY_PLUGIN_FILE' ) || define( 'WP_SENTRY_PLUGIN_FILE', __FILE__ );
 
 /**
- * Resolve the Sentry Plugin DIR url for enqueuing assets
+ * Resolve the WP Sentry Plugin Bootstrap folder location
  *
  * @since 3.0.0
  */
-defined( 'WP_SENTRY_PLUGIN_DIR_URL' ) || define( 'WP_SENTRY_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
-
-/**
- * The composer autoload, absolute unix path.
- *
- * @since 3.0.0
- */
-defined( 'WP_SENTRY_AUTOLOAD_FILE' ) or define( 'WP_SENTRY_AUTOLOAD_FILE', dirname( WP_SENTRY_PLUGIN_FILE ) . '/vendor/autoload.php' );
-
-/**
- * The plugin's config folder location.
- *
- * @since 3.0.0
- */
-defined( 'WP_SENTRY_CONFIG_DIR' ) or define( 'WP_SENTRY_CONFIG_DIR',  dirname( WP_SENTRY_PLUGIN_FILE ) . '//config/' );
-
-/**
- * Define the sentry js script version to load in the browser
- *
- * @since 3.0.0
- */
-defined( 'WP_SENTRY_SCRIPT_VERSION' ) || define( 'WP_SENTRY_SCRIPT_VERSION', '4.6.6' );
-
-/**
- * Re-map deprecated dsn constants to new constant values
- * if they exist.
- *
- * Note that Sentry no longer has "secret"
- * and "public" dsn. The dsn is unified and never includes
- * a secret, so now we are just differentiating between
- * "PHP" and "JS" dsn instead of referencing dsn scope in the name.
- *
- * @since 3.0.0
- */
-if( defined( 'WP_SENTRY_DSN' ) && ! defined( 'WP_SENTRY_PHP_DSN' ) ){
-  define( 'WP_SENTRY_PHP_DSN', WP_SENTRY_DSN );
-}
-
-if( defined( 'WP_SENTRY_PUBLIC_DSN' ) && ! defined( 'WP_SENTRY_JS_DSN' ) ){
-  define( 'WP_SENTRY_JS_DSN', WP_SENTRY_PUBLIC_DSN );
-}
-
-
-/**
- * Run the plugin if:
- *
- * 1. A DSN is defined
- * 2. An instance of the abstract class TrackerBase does not already exist
- *
- * @since 3.0.0
- */
-if( ! class_exists( '\WPSentry\Tracker\TrackerBase' ) ){
-  run_wp_sentry();
-}
+defined( 'WP_SENTRY_BOOTSTRAP_DIR' ) || define( 'WP_SENTRY_BOOTSTRAP_DIR', dirname( WP_SENTRY_PLUGIN_FILE ) . '/bootstrap/' );
 
 /**
  * The function responsible for running the Wordpress Sentry Plugin
@@ -116,27 +57,10 @@ if( ! class_exists( '\WPSentry\Tracker\TrackerBase' ) ){
 function run_wp_sentry(){
 
   // Load dependencies
-  require_once WP_SENTRY_AUTOLOAD_FILE;
+  require_once WP_SENTRY_BOOTSTRAP_DIR . '/define.php';
 
-  // Instantiate the Context provider
-  $context_config = new \WPSentry\Config\Config( WP_SENTRY_CONFIG_DIR . 'context.php' );
-  $context = new \WPSentry\Context\Context( $context_config );
-
-  // Get init runtime configs
-  $init_config = new \WPSentry\Config\Config( WP_SENTRY_CONFIG_DIR . 'init.php' );
-
-  // Instantiate Sentry PHP Tracker if required DSN is defined
-  if( defined( 'WP_SENTRY_PHP_DSN' ) ){
-
-    $sentry_php = new \WPSentry\Tracker\PHP( WP_SENTRY_PHP_DSN, $init_config, $context );
-
-  }
-
-  // Instantiate Sentry JS Tracker if required DSN is defined
-  if( defined( 'WP_SENTRY_JS_DSN' ) ){
-
-    $sentry_js = new \WPSentry\Tracker\JS( WP_SENTRY_JS_DSN, $init_config, $context );
-
-  }
+  require_once WP_SENTRY_BOOTSTRAP_DIR . '/setup.php';
 
 }
+
+run_wp_sentry();
