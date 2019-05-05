76626715fcf5d4a004056e4ed5b5c4aef82de9b4
Christopher Hahn <chris@creativefuse.org>
2019-05-05 10:03:24 -0400

    Use New DSN Constants

--- bootstrap/setup.php
+++ bootstrap/setup.php
@@ -21,13 +21,12 @@ defined( 'WP_SENTRY_EXISTS' ) || exit;
   $init_config = new \WPSentry\Config\Config( WP_SENTRY_CONFIG_DIR . 'init.php' );
 
   // Instantiate Sentry PHP Tracker if required DSN is defined
-  if( defined( 'WP_SENTRY_PHP_DSN' ) ){
-    $sentry_php = new \WPSentry\Tracker\PHP( WP_SENTRY_PHP_DSN, $init_config, $context );
+  if( defined( 'WP_SENTRY_DSN' ) ){
+    $sentry_php = new \WPSentry\Tracker\PHP( WP_SENTRY_DSN, $init_config, $context );
   }
 
   // Instantiate Sentry JS Tracker if required DSN is defined
-  if( defined( 'WP_SENTRY_JS_DSN' ) ){
-    $sentry_js = new \WPSentry\Tracker\JS( WP_SENTRY_JS_DSN, $init_config, $context );
+  if( defined( 'WP_SENTRY_PUBLIC_DSN' ) ){
     $manifest = new \WPSentry\Assets\Manifest;
     $sentry_js = new \WPSentry\Tracker\JS( WP_SENTRY_PUBLIC_DSN, $init_config, $context, $manifest );
   }
