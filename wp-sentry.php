5c50d711839c7a074c7b438314c161fa36d20e4a
Alex Bouma <me@alexbouma.me>
2019-10-07 21:04:47 +0200

    Check the correct class for double load protection

--- wp-sentry.php
+++ wp-sentry.php
@@ -57,7 +57,7 @@ define( 'WP_SENTRY_PLUGIN_FILE', call_user_func( static function () {
 } ) );
 
 // Load dependencies
-if ( ! class_exists( 'WP_Sentry_Tracker_Base' ) ) {
+if ( ! class_exists( WP_Sentry_Version::class ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
