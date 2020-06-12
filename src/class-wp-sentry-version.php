70930927eb1ad19a1aa753482a7c3123442e6f97
Alex Bouma <me@alexbouma.me>
2020-06-12 20:42:59 +0200

    Version bump and release note updates

--- src/class-wp-sentry-version.php
+++ src/class-wp-sentry-version.php
@@ -5,5 +5,5 @@
  */
 final class WP_Sentry_Version {
 	public const SDK_IDENTIFIER = 'sentry.php.wp-sentry-integration';
-	public const SDK_VERSION    = '3.6.0';
+	public const SDK_VERSION    = '3.7.0';
 }
