63f9308c768a0fd7bf504808ad2ebba6e6ebacef
Alex Bouma <me@alexbouma.me>
2020-11-10 16:30:48 +0100

    CS

--- src/trait-wp-sentry-resolves-environment.php
+++ src/trait-wp-sentry-resolves-environment.php
@@ -2,19 +2,19 @@
 
 trait WP_Sentry_Resolve_Environment {
 
-    /**
-     * Retrieve the current environment name from user config or WordPress API.
-     *
-     * @return string
-     */
-    protected function get_environment(): string {
-        $environment = defined( 'WP_SENTRY_ENV' ) ? WP_SENTRY_ENV : null;
+	/**
+	 * Retrieve the current environment name from user config or WordPress API.
+	 *
+	 * @return string
+	 */
+	protected function get_environment(): string {
+		$environment = defined( 'WP_SENTRY_ENV' ) ? WP_SENTRY_ENV : null;
 
-        if ( $environment === null && function_exists( 'wp_get_environment_type' ) ) {
-            $environment = wp_get_environment_type();
-        }
+		if ( $environment === null && function_exists( 'wp_get_environment_type' ) ) {
+			$environment = wp_get_environment_type();
+		}
 
-        return $environment ?? 'unspecified';
-    }
+		return $environment ?? 'unspecified';
+	}
 
 }
