f5c6ddbf7ef663675d555baf7e6c0c9dc5d93016
Alex Bouma <alex@bouma.me>
2022-10-19 09:28:42 +0200

    Version bump and release note updates

--- wp-sentry.php
+++ wp-sentry.php
@@ -4,7 +4,7 @@
  * Plugin Name: WordPress Sentry
  * Plugin URI: https://github.com/stayallive/wp-sentry
  * Description: A (unofficial) WordPress plugin to report PHP and JavaScript errors to Sentry.
- * Version: 6.0.0
+ * Version: 6.1.0
  * Author: Alex Bouma
  * Author URI: https://alex.bouma.dev
  * License: MIT
