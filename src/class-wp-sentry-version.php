92c63e4acbca88707634b6e890aee1b0cd200258
Michi Hoffmann <cleptric@users.noreply.github.com>
2023-03-23 14:00:15 +0100

    Update class-wp-sentry-version.php

--- src/class-wp-sentry-version.php
+++ src/class-wp-sentry-version.php
@@ -4,6 +4,6 @@
  * WordPress Sentry SDK Version.
  */
 final class WP_Sentry_Version {
-	public const SDK_IDENTIFIER = 'sentry.php.wp-sentry-integration';
+	public const SDK_IDENTIFIER = 'sentry.php.wordpress';
 	public const SDK_VERSION    = '6.8.0';
 }
