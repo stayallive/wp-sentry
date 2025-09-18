94088b77df55a8c83258c22fc5dd82c3efa59748
Kevin Pfeifer <info@pfiff.me>
2025-09-18 10:09:30 +0200

    Action scheduler integration (#223)
    Co-authored-by: Alex Bouma <alex@bouma.me>

--- wp-sentry.php
+++ wp-sentry.php
@@ -115,8 +115,14 @@ if ( defined( 'WP_SENTRY_PHP_DSN' ) || defined( 'WP_SENTRY_DSN' ) || defined( 'W
 	}
 
 	if ( ! empty( $sentry_php_tracker_dsn ) || WP_Sentry_Php_Tracker::get_spotlight_enabled() ) {
+		// Error tracker and Sentry SDK bootstrap
 		WP_Sentry_Php_Tracker::get_instance();
+
+		// Performance tracker
 		WP_Sentry_Php_Tracing::get_instance();
+
+		// Action Scheduler integration
+		WP_Sentry_Action_Scheduler_Integration::get_instance();
 	}
 }
 
