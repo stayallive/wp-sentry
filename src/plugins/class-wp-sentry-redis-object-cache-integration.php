9fbd211927c3cad39489b2fbe1e57cb6dce7a092
Alex Bouma <alex@bouma.me>
2026-05-20 11:47:42 +0200

    CR

--- src/plugins/class-wp-sentry-redis-object-cache-integration.php
+++ src/plugins/class-wp-sentry-redis-object-cache-integration.php
@@ -1,48 +1,50 @@
 <?php
 
+use Sentry\State\HubInterface;
+
 /**
  * WordPress Sentry Redis Object Cache Integration
+ *
+ * @see      https://wordpress.org/plugins/redis-cache/
  *
  * @internal This class is not part of the public API and may be removed or changed at any time.
  */
 final class WP_Sentry_Redis_Object_Cache_Integration {
+	/**
+	 * Holds the class instance.
+	 *
+	 * @var WP_Sentry_Redis_Object_Cache_Integration
+	 */
+	private static $instance;
 
-    /**
-     * Holds the class instance.
-     *
-     * @var WP_Sentry_Redis_Object_Cache_Integration
-     */
-    private static $instance;
+	/**
+	 * Get the Sentry Redis Object Cache Integration instance.
+	 *
+	 * @return WP_Sentry_Redis_Object_Cache_Integration
+	 */
+	public static function get_instance(): WP_Sentry_Redis_Object_Cache_Integration {
+		return self::$instance ?: self::$instance = new self;
+	}
 
-    /**
-     * Get the Sentry admin page instance.
-     *
-     * @return WP_Sentry_Redis_Object_Cache_Integration
-     */
-    public static function get_instance(): WP_Sentry_Redis_Object_Cache_Integration {
-        return self::$instance ?: self::$instance = new self;
-    }
+	/**
+	 * Class constructor.
+	 */
+	protected function __construct() {
+		add_action( 'redis_object_cache_error', [ $this, 'handle_redis_cache_failure' ] );
+	}
 
-    /**
-     * WP_Sentry_Redis_Object_Cache_Integration constructor.
-     */
-    protected function __construct() {
-        add_action( 'redis_object_cache_error', array( $this, 'handle_redis_cache_failure' ), 10, 2 );
-    }
-
-    /**
-     * Capture and send Redis Object Cache failures to Sentry.
-     *
-     * @param \Throwable $e The exception that was thrown.
-     * @param string $message The exception message.
-     * @return void
-     */
-    public function handle_redis_cache_failure($e, $message ) {
-        wp_sentry_safe(
-            function ( \Sentry\State\HubInterface $client ) use ( $e ) {
-                $client->captureException( $e );
-            }
-        );
-    }
-
+	/**
+	 * Fires when an object cache related error occurs.
+	 *
+	 * @param \Throwable $e The exception that was thrown.
+	 *
+	 * @return void
+	 */
+	public function handle_redis_cache_failure( Throwable $e ): void {
+		wp_sentry_safe(
+			static function ( HubInterface $client ) use ( $e ) {
+				$client->captureException( $e );
+			}
+		);
+	}
 }
